1dd543792e7f573619f56ece5e673cc01020eee0
Nathan Heidt <nathanielheidt@gmail.com>
2016-02-07 16:38:16 -0800

    updated requirements to inlude RPIO

--- butter_bot/HAL.py
+++ butter_bot/HAL.py
@@ -3,9 +3,16 @@ Hardware abstraction layer for butterbot.  Allows access to the physical hardwar
 
 author: Nathan Heidt
 """
+from RPIO import PWM
 
 class RobotHAL:
 	def __init__(self, hardware="BBB"):
-		pass
+		# Setup PWM and DMA channel 0
+		PWM.setup()
+		PWM.init_channel(0, 20000)
+		self.motors = PWM.servo()
+		
+
+
 	def openGPIO(self):
 		pass
