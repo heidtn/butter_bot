c5de8558ac601e75aa3d15ee3c0a235e44ca83a1
Nathan Heidt <nathanielheidt@gmail.com>
2016-07-13 15:26:57 -0700

    added lcm and updated motor driver pins

--- butter_bot/motor_driver.py
+++ butter_bot/motor_driver.py
@@ -1,29 +1,39 @@
 """this is pulled from Pololu's library for driving motors here: https://github.com/pololu/drv8835-motor-driver-rpi/blob/master/pololu_drv8835_rpi.py"""
 
 import wiringpi2
+import lcm
+from butterbotlcm import motor_t
 
 
+lc = lcm.LCM()
 # Motor speeds for this library are specified as numbers
 # between -MAX_SPEED and MAX_SPEED, inclusive.
 _max_speed = 480  # 19.2 MHz / 2 / 480 = 20 kHz
 MAX_SPEED = _max_speed
 
 io_initialized = False
+
+A1IN = 0
+A2IN = 2
+
+B1IN = 3
+B2IN = 4
+
 def io_init():
   global io_initialized
   if io_initialized:
     return
 
   wiringpi2.wiringPiSetupGpio()
-  wiringpi2.pinMode(17, wiringpi2.GPIO.PWM_OUTPUT)
-  wiringpi2.pinMode(27, wiringpi2.GPIO.PWM_OUTPUT)
+  wiringpi2.pinMode(A1IN, wiringpi2.GPIO.PWM_OUTPUT)
+  wiringpi2.pinMode(B1IN, wiringpi2.GPIO.PWM_OUTPUT)
 
   wiringpi2.pwmSetMode(wiringpi2.GPIO.PWM_MODE_MS)
   wiringpi2.pwmSetRange(MAX_SPEED)
   wiringpi2.pwmSetClock(2)
 
-  wiringpi2.pinMode(22, wiringpi2.GPIO.OUTPUT)
-  wiringpi2.pinMode(23, wiringpi2.GPIO.OUTPUT)
+  wiringpi2.pinMode(A2IN, wiringpi2.GPIO.OUTPUT)
+  wiringpi2.pinMode(B2IN, wiringpi2.GPIO.OUTPUT)
 
   io_initialized = True
 
@@ -44,7 +54,6 @@ class Motor(object):
         if speed > MAX_SPEED:
             speed = MAX_SPEED
 
-        io_init()
         wiringpi2.digitalWrite(self.dir_pin, dir_value)
         wiringpi2.pwmWrite(self.pwm_pin, speed)
 
@@ -52,8 +61,9 @@ class Motors(object):
     MAX_SPEED = _max_speed
 
     def __init__(self):
-        self.motor1 = Motor(12, 5)
-        self.motor2 = Motor(13, 6)
+        self.motor1 = Motor(A1IN, A2IN)
+        self.motor2 = Motor(B1IN, B2IN)
+        io_init()
 
     def setSpeeds(self, m1_speed, m2_speed):
         self.motor1.setSpeed(m1_speed)
@@ -61,11 +71,14 @@ class Motors(object):
 
 motors = Motors()
 
+def my_handler(channel, data):
+  msg = motor_t.decode(data)
+  motors.setSpeeds(msg.leftmotor, msg.rightmotor)
 
 if __name__ == "__main__":
-	import time
-	motors.setSpeeds(0, 0)
-	time.sleep(1)
-	motors.setSpeeds(100, 100)
-	time.sleep(1)
-	motors.setSpeeds(0, 0)
+  subscription = lc.subscribe("EXAMPLE", my_handler)
+  try:
+    while True:
+      lc.handle()
+  except KeyboardInterrupt:
+    pass
---
+++ butter_bot/state_manager.py
@@ -0,0 +1,80 @@
+import speech_recognition as sr
+import lcm
+import subprocess
+import sys
+
+from butterbotlcm import state_t
+
+curstate = "waiting"
+lc = lcm.LCM()
+
+import time
+
+def say(phrase):
+	if phrase == "purpose":
+		subprocess.Popen(['mpg123', 'resources/whatismypurpose.mp3'])
+	elif phrase == "ohmygod":
+		subprocess.Popen(['mpg123', 'resources/ohmygod.mp3'])
+
+def getState():
+	return curstate
+
+def speechCallback(recognizer, audio):
+	try:
+		curstate = getState()
+		print curstate
+		words = recognizer.recognize_google(audio)
+		print("butterbot heard " + words)
+		if "pass the butter" in words and curstate == "inquisition":
+			updateState("search_for_butter")
+		elif "you pass butter" in words and curstate == "inquisition":
+			say("ohmygod")
+			updateState("existential_crisis")
+		else:
+			print("not in dictionary, or bad state")
+			
+	except sr.UnknownValueError:
+		print("didn't catch that..." + repr(sys.exc_info()[0]))
+
+
+def setupSpeech():
+	r = sr.Recognizer()
+	m = sr.Microphone()
+	print("opened Microphone")
+
+	with m as source: r.adjust_for_ambient_noise(source)
+	stop_listening = r.listen_in_background(m, speechCallback)
+
+def updateState(newState):
+	global curstate
+	curstate = newState
+	msg = state_t()
+	msg.timestamp = int(time.time())
+	msg.state = newState
+	lc.publish("BUTTERBOT", msg.encode())
+
+
+if __name__ == "__main__":
+	global curstate
+	print("Initializing speech module")
+	setupSpeech()
+	say("purpose")
+	updateState("inquisition")
+	print curstate
+
+	while True:
+
+		if curstate == "waiting":
+			pass
+		elif curstate == "inquisition":
+			pass
+		elif curstate == "search_for_butter":
+			pass
+		elif curstate == "go_to_butter":
+			pass
+		elif curstate == "grab_butter":
+			pass
+		elif curstate == "return_butter":
+			pass
+		elif curstate == "existential_crisis":
+			pass		
